9072b9f8672b975458f01a15fabcc61a9c4f1035
Peter Molettiere <peter.molettiere@gmail.com>
2023-12-01 15:35:03 -0500

    Simplifying for debug

--- arduino/sketch/sketch.ino
+++ arduino/sketch/sketch.ino
@@ -24,8 +24,8 @@ const int SDIO = D11;
 const int SCLK = D13;
 
 // analog pins
-const int unused0 = A0;
-const int tank1 = A1;      // input from tank sensor
+const int tank1 = A0;          // input from tank sensor
+const int unused1 = A1;      
 const int unused2 = A2;
 const int unused3 = A3;
 const int unused4 = A4;
@@ -38,36 +38,36 @@ const float ADC_TO_VOLTAGE_FACTOR = BOARD_VOLTAGE / 1024;
 
 // eight bit commands
 // A A A A  C C D D 
-int addressMask8 = 0xF0;
-int commandMask8 = 0XC;
-int dataMask8 = 0x3;
+const int addressMask8 = 0xF0;
+const int commandMask8 = 0XC;
+const int dataMask8 = 0x3;
 
 // sixteen bit commands
 // A A A A  C C D D    D D D D  D D D D
-int addressMask16 = 0xF000;
-int commandMask16 = 0XC00;
-int dataMask16 = 0x3FF;
+const int addressMask16 = 0xF000;
+const int commandMask16 = 0XC00;
+const int dataMask16 = 0x3FF;
 
 // MCP4151 commands
-byte CMD_READ = 0x3;
-byte CMD_WRITE = 0x0;
-byte CMD_INCREMENT = 0x1;
-byte CMD_DECREMENT = 0x2;
+const byte CMD_READ = 0x3;
+const byte CMD_WRITE = 0x0;
+const byte CMD_INCREMENT = 0x1;
+const byte CMD_DECREMENT = 0x2;
 
 // MCP4151 CS constants
-byte CS_SELECTED = LOW;
-byte CS_UNSELECTED = HIGH;
+const byte CS_SELECTED = LOW;
+const byte CS_UNSELECTED = HIGH;
 
 // MCP4151 addresses
-byte ADR_W1 = 0x0;  // wiper 1
+const byte ADR_W1 = 0x0;  // wiper 1
 
 // MCP4151 SPI Settings
-int SPI_BAUD = 10000000;
+const int SPI_BAUD = 10000000;
 
 
 // Digital pot number of steps
-byte DPOT_STEPS = 256;  // could be 127?
-float DPOT_VOLTS_PER_STEP =  DPOT_STEPS / BOARD_VOLTAGE;
+const byte DPOT_STEPS = 256;  // could be 127?
+const float DPOT_VOLTS_PER_STEP =  DPOT_STEPS / BOARD_VOLTAGE;
 
 /*
   Shifts the bottom four bits of the address, the bottom two 
@@ -122,36 +122,36 @@ void loop() {
   #ifdef DEBUG
     Serial.print("loop(): ");
     Serial.print("reading: ");
-    Serial.print(reading, HEX);
+    Serial.print(reading);
     Serial.print(", voltage: ");
     Serial.print(voltage);
     Serial.print("dpotSetting: ");
     Serial.println(dpotSetting);
   #endif
 
-  // set the digital potentiometer
-  int cmd = twoByteCommand(ADR_W1, CMD_WRITE, dpotSetting);
-  #ifdef DEBUG
-    Serial.print("loop(): constructed cmd "); Serial.println( cmd, HEX );
-  #endif
+  // // set the digital potentiometer
+  // int cmd = twoByteCommand(ADR_W1, CMD_WRITE, dpotSetting);
+  // #ifdef DEBUG
+  //   Serial.print("loop(): constructed cmd "); Serial.println( cmd, HEX );
+  // #endif
 
-  #ifdef DEBUG
-    Serial.println ("loop(): begin SPI" );
-  #endif
-  digitalWrite(CS, CS_SELECTED); // select
-  SPI.begin();
-  SPI.beginTransaction( SPISettings(SPISettings(SPI_BAUD, MSBFIRST, SPI_MODE0)) );
-  SPI.transfer16(cmd);
-  SPI.endTransaction();
-  digitalWrite(CS, CS_UNSELECTED);
-  #ifdef DEBUG
-    Serial.println ("loop(): end SPI" );
-  #endif
+  // #ifdef DEBUG
+  //   Serial.println ("loop(): begin SPI" );
+  // #endif
+  // digitalWrite(CS, CS_SELECTED); // select
+  // SPI.begin();
+  // SPI.beginTransaction( SPISettings(SPISettings(SPI_BAUD, MSBFIRST, SPI_MODE0)) );
+  // SPI.transfer16(cmd);
+  // SPI.endTransaction();
+  // digitalWrite(CS, CS_UNSELECTED);
+  // #ifdef DEBUG
+  //   Serial.println ("loop(): end SPI" );
+  // #endif
 
-  #ifdef DEBUG
-    delay(10000);
-    Serial.println ("...Exiting loop.");
-  #endif
+  // #ifdef DEBUG
+  //   delay(1000);
+  //   Serial.println ("...Exiting loop.");
+  // #endif
 }
 
 void setup() {
@@ -168,7 +168,8 @@ void setup() {
   pinMode(CS, OUTPUT);
   pinMode(SDIO, OUTPUT);
   pinMode(SCLK, OUTPUT);
-  pinMode(tank1, INPUT);
+  //pinMode(tank1, INPUT);  // don't need to set pin mode for analog pins
+  //analogReference(EXTERNAL);
 
   #ifdef DEBUG
     Serial.println ("Exiting setup.");
