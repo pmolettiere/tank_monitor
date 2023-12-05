2b4da830e9190ae157676700e311596463636e52
Peter Molettiere <peter.molettiere@gmail.com>
2023-12-05 07:58:43 -0500

    Moving libraries into sketch folder. Changing sketch to use mcp4xxx library for SPI comms.

--- arduino/sketch/sketch.ino
+++ arduino/sketch/sketch.ino
@@ -1,4 +1,7 @@
 #include <SPI.h>
+#include <mcp4xxx.h>
+
+using namespace icecave::arduino;
 
 // #define DEBUG
 // #define TEST
@@ -41,89 +44,17 @@ const float BOARD_VOLTAGE = 5.0;  // 5.0 is the voltage for the Arduino UNO.
 // The factor used to convert Arduino analogRead() inputs directly to voltage.
 const float ADC_TO_VOLTAGE_FACTOR = BOARD_VOLTAGE / 1024.0;
 
-// eight bit commands
-// A A A A  C C D D 
-const int addressMask8 = 0xF;
-const int commandMask8 = 0x3;
-const int dataMask8 =    0x3;
-
-// sixteen bit commands
-// A A A A  C C D D    D D D D  D D D D
-const long addressMask16 = 0x00F;
-const long commandMask16 = 0x003;
-const long dataMask16 =    0x3FF;
-
-// MCP4151 commands
-const byte CMD_READ = 0x3;
-const byte CMD_WRITE = 0x0;
-const byte CMD_INCREMENT = 0x1;
-const byte CMD_DECREMENT = 0x2;
-
-// MCP4151 CS constants
-const byte CS_SELECTED = LOW;
-const byte CS_UNSELECTED = HIGH;
-
-// MCP4151 addresses
-const byte ADR_W1 = 0x0;  // wiper 1
-
-// MCP4151 SPI Settings
-const int SPI_BAUD = 10000000;
-
+// MCP4151
+MCP4XXX* pot1;
 
 // Digital pot number of steps
-const int DPOT_STEPS = 256;  // could be 127?
+const int DPOT_STEPS = 255;  
 const float DPOT_VOLTS_PER_STEP =  DPOT_STEPS / BOARD_VOLTAGE;
 
 // LED color levels as dpotSetting values
 const int GREEN = 155;
 const int YELLOW = 104;
 const int FLASH = 53;
-
-/*
-  Shifts the bottom four bits of the address, the bottom two 
-  bits of the command, and the bottom two bits of the data into
-  a one byte command word of the format:
-            A A A A  C C D D
-*/
-byte oneByteCommand(byte address, byte command, byte data) {
-  return -1; // needs to be reimplemented along the lines of twoByteCommand.
-}
-
-void debugLog(String name, long value, long masked, long shifted) {
-   #ifdef DEBUG
-    Serial.print("twoByteCommand(): "); Serial.println(name);
-    Serial.print(" value: "); Serial.println(value, HEX); 
-    Serial.print(" maskd: "); Serial.println(masked, HEX);
-    Serial.print(" shift: "); Serial.println(shifted, HEX);
-  #endif 
-}
-/*
-  Shifts the bottom four bits of the address, the bottom two 
-  bits of the command, and the bottom ten bits of the data into
-  a two byte command word of the format:
-            A A A A  C C D D    D D D D  D D D D
-*/
-int twoByteCommand(byte address, byte command, int data) {
-
-  long maskedAddress = address & addressMask16;
-  long maskedCommand = command & commandMask16;
-  long maskedData = data & dataMask16;
-
-  long shiftedAddress = maskedAddress << 12;
-  long shiftedCommand = maskedCommand << 10;
-  long shiftedData = maskedData;
-
-  int result = shiftedAddress | shiftedCommand | shiftedData;
-  
-  // #ifdef DEBUG
-  //   debugLog("address", address, maskedAddress, shiftedAddress);
-  //   debugLog("command", command, maskedCommand, shiftedCommand);
-  //   debugLog("data", data, maskedData, shiftedData);
-  //   Serial.print(" result: "); Serial.println(result, HEX);
-  // #endif
-
-  return result;
-}
 
 int convertReadingToDPotSetting(int reading) { 
   float voltage = reading * ADC_TO_VOLTAGE_FACTOR;
@@ -156,20 +87,12 @@ void loop() {
       // need to implement flash here
     }
   }
-
-  // set the digital potentiometer
-  int cmd = twoByteCommand(ADR_W1, CMD_WRITE, dpotSetting);
-  Serial.print(", cmd "); Serial.println( cmd, HEX );
   
   #ifdef DEBUG
     Serial.println ("loop(): begin SPI" );
   #endif
-  digitalWrite(CS, CS_SELECTED); // select
-  SPI.begin();
-  SPI.beginTransaction( SPISettings(SPISettings(SPI_BAUD, MSBFIRST, SPI_MODE0)) );
-  SPI.transfer16(cmd);
-  SPI.endTransaction();
-  digitalWrite(CS, CS_UNSELECTED);
+  // set the digital potentiometer
+  pot1->set(dpotSetting);
   #ifdef DEBUG
     Serial.println ("loop(): end SPI" );
   #endif
@@ -186,7 +109,6 @@ void setup() {
 
   #ifdef TEST
     Serial.println( "Executing tests...");
-    testTwoByteCommand();
     testConvertReadingToDPotSetting();
   #endif
 
@@ -197,6 +119,12 @@ void setup() {
   pinMode(gled, OUTPUT);
   pinMode(yled, OUTPUT);
   pinMode(rled, OUTPUT);
+
+  // initialize the digital potentiometer connection
+  pot1 = new MCP4XXX(CS);
+  // Move the wiper to the lowest value
+  pot1->set(0);
+
   //pinMode(tank1, INPUT);  // don't need to set pin mode for analog pins
   //analogReference(EXTERNAL);
 
@@ -205,34 +133,6 @@ void setup() {
 
 
 #ifdef TEST
-
-void testTwoByteCommand() {
-  const int numTests = 6;
-  // address, command, data, result
-  int tests[numTests][4] = {
-    {0, 0, 0, 0},
-    {addressMask16, commandMask16, dataMask16, 0xFFFFFFF},
-    {0x1, 0x1, 0x235, 0x1635},
-    {0x2, 0x2, 0x235, 0x2A35},
-    {0x4, 0x1, 0x235, 0x4635},
-    {0x8, 0x2, 0x235, 0x8A35}
-  };
-
-  Serial.println("testTwoByteCommand():");
-
-  for( int t=0; t < numTests; t++) {
-    byte address = tests[t][0];
-    byte command = tests[t][1];
-    int data = tests[t][2];
-    int correct = tests[t][3];
-    int result = twoByteCommand(address, command, data);
-    if( result == correct ) {
-        Serial.print("passed "); Serial.println(t, DEC);
-    } else {
-        Serial.print("failed "); Serial.println(t, DEC);
-    }
-  }
-}
 
 void testConvertReadingToDPotSetting() {
   const int numTests = 32;
