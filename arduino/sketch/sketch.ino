6fb0aa1730b0260c98b1a509925ae1d522bcf6d2
Peter Molettiere <peter.molettiere@gmail.com>
2024-01-03 19:29:26 -0500

    Three tank support.

--- arduino/sketch/sketch.ino
+++ arduino/sketch/sketch.ino
@@ -1,6 +1,6 @@
-#define DEBUG          // Enables serial debug mussaging
-//#define LOG_WRITE
-#define TEST           // Executes unit test code at the end of setup
+#define DEBUG            // Enables serial debug mussaging
+#define LOG_WRITE
+//#define TEST           // Executes unit test code at the end of setup
 
 // Hardware constants
 // digital pins
@@ -18,13 +18,63 @@ const int D10 = 10;
 const int D11 = 11;
 const int D12 = 12;
 const int D13 = 13;
+const int D14 = 14;
+const int D15 = 15;
+const int D16 = 16;
+const int D17 = 17;
+const int D18 = 18;
+const int D19 = 19;
+const int D20 = 20;
+const int D21 = 21;
+const int D22 = 22;
+const int D23 = 23;
+const int D24 = 24;
+const int D25 = 25;
+const int D26 = 26;
+const int D27 = 27;
+const int D28 = 28;
+const int D29 = 29;
+const int D30 = 30;
+const int D31 = 31;
+const int D32 = 32;
+const int D33 = 33;
+const int D34 = 34;
+const int D35 = 35;
+const int D36 = 36;
+const int D37 = 37;
+const int D38 = 38;
+const int D39 = 39;
+const int D40 = 40;
+const int D41 = 41;
+const int D42 = 42;
+const int D43 = 43;
+const int D44 = 44;
+const int D45 = 45;
+const int D46 = 46;
+const int D47 = 47;
+const int D48 = 48;
+const int D49 = 49;
+const int D50 = 50;
+const int D51 = 51;
+const int D52 = 52;
+
 // analog pins
-const int sensor = A0;      // input from tank sensor
-const int unused1 = A1;
-const int unused2 = A2;
-const int gled = A3;        // Green LED
-const int yled = A4;        // Yellow LED
-const int rled = A5;        // Red LED
+const int unused0 = A0;
+const int tank1 = A1;      // input from tank 1 sensor
+const int tank2 = A2;      // input from tank 2 sensor
+const int tank3 = A3;      // input from tank 3 sensor
+const int rled2 = A4;        // Red LED, Tank 2
+const int gled2 = A5;        // Green LED, Tank 2        
+const int yled2 = A6;        // Yellow LED, Tank 2        
+const int unused7 = A7;        
+const int rled3 = A8;        // Red LED, Tank 3     
+const int gled3 = A9;        // Green LED, Tank 3        
+const int yled3 = A10;        // Yellow LED, Tank 3
+const int switchOut = A11;        
+const int switchIn = A12;        
+const int rled1 = A13;        // Red LED, Tank 1
+const int gled1 = A14;        // Green LED, Tank 1
+const int yled1 = A15;        // Yellow LED, Tank 1
 
 // Algorithm constants
 // Total period over which to average the tank level for each update, in milliseconds
@@ -33,21 +83,52 @@ const int averagePeriodMs = 1000;
 const int numReadings = 10;  
 // Number of ms to delay to allow ADC to stabilize before taking each reading.
 const int delayMs = averagePeriodMs / numReadings;     
+
+// the number of tanks to be monitored
+const int numTanks = 3;
 // Number of voltage buckets and outputs to track
-const int numBuckets = 9;  
+const int numBuckets = 7; 
+
+// the sensor pin for each tank
+const int sensor[numTanks] = { tank1, tank2, tank3 };
+
 // The voltage limits for each bucket. vLimit[bucketNumber] is the lower limit for each bucket, and 
 // vLimit[bucketNumber+1] is the upper limit for each bucket.
-const float vLimit[numBuckets+1] = { 0.1, 0.3, 1.0, 1.75, 2.2, 2.8, 3.5, 4.0, 4.6, 6.0 }; 
+const float vLimit[numTanks][numBuckets] = {
+  { 0.1, 1.0, 1.5, 2.0, 2.5, 3.5, 4.0 },
+  { 0.1, 1.0, 1.5, 2.0, 2.5, 3.5, 4.0 },
+  { 0.1, 1.0, 1.5, 2.0, 2.5, 3.5, 4.0 }
+};
+
 // The led color to be illuminated when the voltage falls within the limits for each bucket.
-const int ledColor[numBuckets] = { rled, yled, gled, gled, gled, gled, gled, gled, gled };
+const int bucketColor[numTanks][numBuckets+1] = {
+  { rled1, rled1, yled1, yled1, gled1, gled1, gled1, gled1 },
+  { rled2, rled2, yled2, yled2, gled2, gled2, gled2, gled2 },
+  { gled3, gled3, gled3, gled3, yled3, yled3, rled3, rled3 }
+};
+
 // The pin to be energized when the voltage falls within the limits for each bucket. This pin will
 // energize the appropriate relay to set the desired resistance.
-const int relay[numBuckets] = { D2, D3, D4, D5, D6, D7, D8, D9, D10 };
+const int relay[numTanks][numBuckets+1] = { 
+  { D13, D12, D11, D10,  D9,  D8,  D7,  D6 }, 
+  { D22, D23, D24, D25, D26, D27, D28, D29 }, 
+  { D33, D35, D37, D39, D41, D43, D45, D47 } 
+};
+
+// The alarm LED for each tank
+const int ledColor[numTanks][3] = {
+  { rled1, yled1, gled1 },
+  { rled2, yled2, gled2 },
+  { rled3, yled3, gled3 }
+};
+const int red = 0;
+const int yellow = 1;
+const int green = 2;
 
 // algorithm variables
 // The last bucket to be set. Allows us to clear the last bucket when the bucket changes, and to only 
 // change led and relay states when the bucket changes.
-int lastBucket = 0;
+int lastBucket[numTanks] = { 0, 0, 0};
 
 // Error constants
 // Voltage below lower limit.
@@ -75,20 +156,14 @@ const float ADC_TO_VOLTAGE_FACTOR = BOARD_VOLTAGE / 1024;
 // is not within the limits defined by vLimit, either below or equal to vLimit[0] or above vLimit[numBuckets+1],
 // then either ERR_LOW_VOLTAGE or ERR_HIGH_VOLTAGE are returned respectively. All valid bucket indexes
 // are non-negative, and all errors returned are negative. 
-int getBucketForVoltage(float v) {
+int getBucketForVoltage(float v, int tank) {
   #ifdef TEST
     v = round( v * 100.0 ) / 100.0; // deals with float weirdness during tests
   #endif
-  if ( v <= vLimit[0] ) {
-    #ifdef DEBUG
-      Serial.print("getBucketForVoltage returning ERR_LOW_VOLTAGE for "); Serial.println(v);
-    #endif
-    return ERR_LOW_VOLTAGE;
-  }
-
   // Loop over all the vLimits
-  for (int i=0; i<=numBuckets; i++) {
-    if (v > vLimit[i] && v <= vLimit[i+1]) {
+  int n = numBuckets;
+  for (int i=0; i<numBuckets; i=i+1) {
+    if ( v <= vLimit[tank][i]) {
         #ifdef DEBUG
           Serial.print("getBucketForVoltage returning bucket "); Serial.print(i); Serial.print(" for voltage "); Serial.println(v);
         #endif
@@ -97,35 +172,35 @@ int getBucketForVoltage(float v) {
   }
 
   #ifdef DEBUG
-    Serial.print("getBucketForVoltage returning ERR_HIGH_VOLTAGE for "); Serial.println(v);
+    Serial.print("getBucketForVoltage returning bucket 7 for voltage "); Serial.println(v);
   #endif
-  return ERR_HIGH_VOLTAGE;
+  return numBuckets;
 }
 
 // Reads the voltage from the sensor pin numReadings times, with a delayMs delay between each reading. The 
 // intention behind this delay is to allow the ADC to stabilize between readings, and the multiple readings 
 // are averaged to present a more stabilized reading in the face of tank level fluctuations due to motion
 // within the tank.
-float readAvgVoltage(int numReadings, int delayMs) { 
+float readAvgVoltage(int numReadings, int delayMs, int pin) { 
   int total = 0;               // Sum of all readings
   float avgVoltage = 0;        // Averaged value
 
   // totals all ADC outputs in the range of 0-1023
   for (int i = 0; i < numReadings; i++) {
     delay(delayMs);                    // delay before reading the sensor, so all readings have time to settle
-    total += analogRead(sensor);    
+    total += analogRead(pin);    
   }
   // converts the total of all digital ADC outputs into a total mV value, converting from int to float,
   // and divides by the number of readings to obtain an average mV value.
   avgVoltage = (total * ADC_TO_VOLTAGE_FACTOR) / numReadings;
   #ifdef DEBUG
-    Serial.print("readAvgVoltage() returning "); Serial.println( avgVoltage);
+    Serial.print("readAvgVoltage() returning "); Serial.print(avgVoltage); Serial.print(" for pin "); Serial.println(pin);
   #endif
   return avgVoltage;
 }
 
 // Debugging method to log all digital writes.
-void logWrite(int pin, int value) {
+void  logWrite(int pin, int value) {
   #ifdef LOG_WRITE && DEBUG 
       Serial.print("digitalWrite( pin " ); Serial.print(pin); Serial.print(", "); Serial.print(value); Serial.println(")");
   #endif
@@ -134,30 +209,30 @@ void logWrite(int pin, int value) {
 
 // Manipulate the UI appropriately to indicate an error has occurred. Currently only turns the red LED on
 // then off once. 
-void alarm() {                 // flash red led if volume falls below 7%
+void alarm(int tank) {                 // flash red led if volume falls below 7%
   #ifdef DEBUG
     Serial.println( "Alarm triggered!" );
   #endif
-  logWrite(rled, HIGH);
+  logWrite(ledColor[tank][red], HIGH);
   delay (1000);
-  logWrite(rled, LOW);
+  logWrite(ledColor[tank][red], LOW);
   delay (1000);
   #ifdef DEBUG
     Serial.println( "Alarm exiting." );
   #endif
 }
 
-void setBucket(int b, bool inBucket) {
+void setBucket(int b, bool inBucket, int tank) {
   #ifdef DEBUG
-    Serial.print("setBucket() setting bucket "); Serial.print(b); Serial.println(inBucket ? " to true" : " to false");
+    Serial.print("setBucket() setting bucket "); Serial.print(b); Serial.print(inBucket ? " to true" : " to false"); Serial.print(" for tank "); Serial.println(tank);
   #endif
   // set the relay HIGH if we're in the current voltage bucket, otherwise set LOW
-  logWrite( relay[b], inBucket ? HIGH : LOW );
+  logWrite( relay[tank][b], inBucket ? HIGH : LOW );
   // set each led to HIGH if the color matches the current bucket, otherwise set LOW
-  int currentColor = ledColor[b];
-  logWrite( rled, (inBucket && rled == currentColor) ? HIGH : LOW );
-  logWrite( yled, (inBucket && yled == currentColor) ? HIGH : LOW );
-  logWrite( gled, (inBucket && gled == currentColor) ? HIGH : LOW );
+  int currentColor = bucketColor[tank][b];
+  logWrite( ledColor[tank][red], (inBucket && ledColor[tank][red] == currentColor) ? HIGH : LOW );
+  logWrite( ledColor[tank][yellow], (inBucket && ledColor[tank][yellow] == currentColor) ? HIGH : LOW );
+  logWrite( ledColor[tank][green], (inBucket && ledColor[tank][green] == currentColor) ? HIGH : LOW );
 }
 
 // This is the arduino entry point. It is called repeatedly from the OS. Each invocation will read
@@ -167,45 +242,29 @@ void loop() {
   #ifdef DEBUG
     Serial.println ("loop()");
   #endif
-  float v = readAvgVoltage(numReadings, delayMs);
-  int vBucket = getBucketForVoltage(v);
 
-  if( vBucket == lastBucket ) {
-    return; // do nothing if the bucket hasn't changed.
-  }
+  for( int tank=0; tank<numTanks; tank++ ) {
+    float v = readAvgVoltage(numReadings, delayMs, sensor[tank]);
+    int vBucket = getBucketForVoltage(v, tank);
 
-  switch( vBucket ) {
-    case ERR_LOW_VOLTAGE : {
+    if( vBucket == lastBucket[tank] ) {
+      continue; // do nothing if the bucket hasn't changed.
+    }
+
+    if( vBucket < 0 || vBucket > numBuckets ) {
       #ifdef DEBUG
-        Serial.println("ERR_LOW_VOLTAGE detected.");
+        Serial.print("Unknown error detected: "); Serial.println(vBucket);
       #endif
-      setBucket(lastBucket, false);
-      alarm();
-      break;
+      continue;
     }
-    case ERR_HIGH_VOLTAGE : {
-      #ifdef DEBUG
-        Serial.println("ERR_HIGH_VOLTAGE detected.");
-      #endif
-      setBucket(lastBucket, false);
-      logWrite (ERR_HIGH_VOLT_PIN, HIGH);      // sensor voltage too high
-      break;
-    }
-    default : {
-      if( vBucket < 0 || vBucket >= numBuckets ) {
-        #ifdef DEBUG
-          Serial.print("Unknown error detected: "); Serial.println(vBucket);
-        #endif
-        break;
-      }
-      // normal operation
-        #ifdef DEBUG
-          Serial.print("Switching bucket state from "); Serial.print(lastBucket); Serial.print(" to "); Serial.println(vBucket);
-        #endif
-      setBucket(lastBucket, false);
-      setBucket(vBucket, true);
-      lastBucket = vBucket;
-    }
+
+    // normal operation
+    #ifdef DEBUG
+      Serial.print("Switching bucket state from "); Serial.print(lastBucket[tank]); Serial.print(" to "); Serial.print(vBucket); Serial.print(" for tank "); Serial.println(tank);
+    #endif
+    setBucket(lastBucket[tank], false, tank);
+    setBucket(vBucket, true, tank);
+    lastBucket[tank] = vBucket;
   }
 }
 
@@ -223,16 +282,19 @@ void setup() {
 
   // initialize pins
   // all relay output pins
-  for( int i=0; i<numBuckets; i++) {
-    pinMode( relay[i], OUTPUT);
+  for( int tank=0; tank < numTanks; tank++ ) {
+    for( int i=0; i<numBuckets; i++) {
+      pinMode( relay[tank][i], OUTPUT);
+    }
+    pinMode(sensor[tank], INPUT);       // Tank sensor input
+    // LEDs
+    pinMode(ledColor[tank][red], OUTPUT);        
+    pinMode(ledColor[tank][yellow], OUTPUT);        
+    pinMode(ledColor[tank][green], OUTPUT);
+
+    // high voltage error pin
+    pinMode(ERR_HIGH_VOLT_PIN, OUTPUT);
   }
-  pinMode(sensor, INPUT);       // Tank sensor input
-  // LEDs
-  pinMode(rled, OUTPUT);        
-  pinMode(yled, OUTPUT);        
-  pinMode(gled, OUTPUT);
-  // high voltage error pin
-  pinMode(ERR_HIGH_VOLT_PIN, OUTPUT);
   #ifdef DEBUG
     Serial.println ("Exiting setup.");
   #endif
@@ -247,15 +309,16 @@ void testGetBucketForVoltage() {
     Serial.println("testGetBucketForVoltage()");
   #endif
 
-//const float vLimit[numBuckets+1] = { 0.1, 0.5, 1.0, 1.2, 1.75, 2.2, 2.4, 3.4, 4.0, 4.6, 6.0 }; 
+ // voltages! { 0.1, 1.0, 1.5, 2.0, 2.5, 3.5, 4.0 },
+ // buckets!  {   0    1.   2.   3.   4.   5.   6.   7}
 
   int testCaseLen = 11; // length of the testCase array
   float testCase[] = {0.0, 0.1, 0.3, 0.5, 0.6, 1.0, 1.1, 1.2, 3.0, 6.0, 7.0 };  // the test inputs
-  int expected[] =   { -1,  -1,   0,   0,   1,   1,   2,   2,   6,  9,  -2 };   // and expected output values for each input
+  int expected[] =   {  0,   0,   1,   1,   1,   1,   2,   2,   5,   7,   7 };  // and expected output values for each input
 
   bool pass = true;
   for(int i=0; i<testCaseLen; i++) {
-    int actual = getBucketForVoltage(testCase[i]);
+    int actual = getBucketForVoltage(testCase[i], 0);
     if(expected[i] != actual) {
       Serial.print( "   Case "); Serial.print(i); Serial.print(" expected "); Serial.print(expected[i]); Serial.print(" != "); Serial.println(actual);
       pass = false;
